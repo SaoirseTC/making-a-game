0d809c89ec301e1b5270ec4ec4a2eb99eaf9f80d
SaoirseTC <91208294+SaoirseTC@users.noreply.github.com>
2021-11-01 14:28:24 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(score_a)
+    score_a += 0 + 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(score_a)
+    score_a += 3
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(score_a)
+    score_a += 2
+})
+let score_a = 0
+score_a = 0
